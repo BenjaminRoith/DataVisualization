20f9970e2284d6890b1cb52e595c79cb954788b8
Benjamin Roith <benroith@bens-mbp-10.home>
2019-04-02 17:05:41 -0400

    initial commit

---
+++ DataVisualization.js
@@ -0,0 +1,19 @@
+
+
+function preload(){
+}
+
+function setup(){
+createCanvas(500,500);
+background(0, 7, 0);
+}
+
+function draw(){
+}  
+
+
+
+
+/*function preload(){
+    img2 = loadImage('Images/Global_Map.png');
+}*/
