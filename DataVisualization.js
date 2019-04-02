05d4fcf734df2ecc99fab6bac4308ae95218c995
Benjamin Roith <benroith@bens-mbp-10.home>
2019-04-02 17:21:05 -0400

    added data

--- DataVisualization.js
+++ DataVisualization.js
@@ -1,16 +1,38 @@
 
+let table;
+var friends;
 
 function preload(){
+    table = loadTable('data/friends.csv', 'header');
 }
 
 function setup(){
-createCanvas(500,500);
-background(0, 7, 0);
+   createCanvas(800,800);
+   loadData();
 }
 
 function draw(){
-}  
+    background(0);
+    for (var i = 0; i < friends.length; i++){
+        friends[i].display();
+    }
+}
 
+function loadData(){
+    friends = [];
+    
+    for (var i = 0; i < table.getRowCount(); i++){
+        var row = table.getRow(i);
+        var n = row.get("name");
+        var d = row.get("d");
+        var m = row.get("m");
+        var y = row.get("y");
+        
+    friends[i] = new Friend(n, m, d, y);
+
+    }
+
+}
 
 
 
